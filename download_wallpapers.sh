a5950eb2d9d1bc843172eadaa605507144ae3df4
Roger Almeida <roger.eduardo@gmail.com>
2011-11-11 17:34:04 -0200

    Downloading pages using curl instead of wget

--- download_wallpapers.sh
+++ download_wallpapers.sh
@@ -1,21 +1,11 @@
-COUNT=1
-rm -r paginas
 rm -r pictures
-mkdir paginas
 mkdir pictures
+cd pictures
 for page_sufix in  `curl http://www.wizards.com/magic/magazine/downloads.aspx?x=mtg/daily/downloads/wallpapers |grep href | grep /magic/magazine/article.aspx | sed 's/^.*<a href="//' | sed 's/".*$//'`
 do
-	echo $page_sufix
-	cd paginas
-	NAME="pagina_$COUNT.html"
-	wget -O $NAME "www.wizards.com$page_sufix"
-	COUNT=$(($COUNT+1))
-	for image in `grep href $NAME | grep x1024 | grep -o -E 'href="([^"]*)"' | cut -f2 -d'='| sed  -e 's/"//g'`
+	for image in `curl "www.wizards.com$page_sufix" | grep href | grep x1024 | grep -o -E 'href="([^"]*)"' | cut -f2 -d'='| sed  -e 's/"//g'`
 	do
-		cd ../pictures
 		echo "Downloading $image"
 		wget "$image"
-		cd ../paginas
 	done
-	cd ..
 done
