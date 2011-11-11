9a04aba406fe8d95f3455625a7507268353a3678
Roger Almeida <roger.eduardo@gmail.com>
2011-11-11 21:43:47 -0200

    Do not download existing files

--- download_wallpapers.sh
+++ download_wallpapers.sh
@@ -5,7 +5,10 @@ for page_sufix in  `curl http://www.wizards.com/magic/magazine/downloads.aspx?x=
 do
 	for image in `curl "www.wizards.com$page_sufix" | grep href | grep x1024 | grep -o -E 'href="([^"]*)"' | cut -f2 -d'='| sed  -e 's/"//g'`
 	do
-		echo "Downloading $image"
-		wget "$image"
+		echo "$image" | awk -F"/" '{print $NF }'
+		if [ ! -f `echo "$image" | awk -F"/" '{print $NF }'` ]; then
+			echo "Downloading $image"
+			wget "$image"
+		fi
 	done
 done
